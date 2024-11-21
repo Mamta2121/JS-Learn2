f7d184c3587cb999b5338f242bfb00dcf1f97ce3
Mamta2121 <mamtarajera2121@gmail.com>
2024-11-21 11:49:31 +0530

    Data Type Conversion. How it differs in case of relational and equality operators.

--- 01_Basics/dataTypeComparison.js
+++ 01_Basics/dataTypeComparison.js
@@ -14,6 +14,9 @@ console.log( 1 == 2)
 */
 
 
+/* Strings like "1" are converted to numbers if possible.
+Non-numeric strings like "2ab", "null", and "undefined" become NaN.
+comparisons with NaN are always false*/
 //string any other other type
 //different data tyep string and number being compared
 
@@ -24,7 +27,11 @@ console.log( "null" < 2)
 console.log( "undefined" < 2)
 
 
-//Avoid this type of comparison=> Not a clean Code Hence not recommended
+//Avoid this type of comparison=> Not a clean Code Hence not recommended 
+----->Relational operators (<, <=, >, >=) trigger numeric conversion for null. This is why null is treated as 0 in comparisons like null < 2 and null >= 0.
+----->Equality operators (== and ===) do not trigger numeric conversion for null. Instead:
+null == undefined is true because they are considered "equal" in loose equality.
+null == 0 is false because null is not converted to 0.
 
 //null
 console.log( null < 2)
@@ -37,6 +44,28 @@ console.log( null >= 0)
 console.log(undefined == 0)
 console.log(undefined > 0)
 console.log(undefined < 0)
+
+//comparing null and undefined 
+--->Special Rule for null and undefined:
+
+In loose equality (==), null is only equal to undefined and no other value.
+null == undefined evaluates to true
+
+--->Why?
+When using relational operators (<, <=, >, >=), JavaScript attempts to convert the values to numbers for comparison. Here's how it processes null < undefined:
+
+undefined as a Number:
+When undefined is converted to a number, it becomes NaN.
+
+null as a Number:
+When null is converted to a number, it becomes 0.
+
+Comparison with NaN:
+Any comparison (<, >, <=, >=) involving NaN always returns false.
+Since undefined is converted to NaN, the result of null < undefined is false
+
+console.log( null == undefined); //true
+console.log(null < undefined); //false
 
 
 //===
