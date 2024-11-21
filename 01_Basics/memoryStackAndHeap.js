e7b8d1fc5064e41b1b77d857c04cf3108ee813d7
Mamta2121 <mamtarajera2121@gmail.com>
2024-11-21 12:13:45 +0530

    Memory Allocation of primitive and non-primitive

--- 01_Basics/memoryStackAndHeap.js
+++ 01_Basics/memoryStackAndHeap.js
@@ -1,6 +1,20 @@
 
 //stack=>Primitive
+/*
+-->Primitive Types (Stack Memory)
+Primitive data types (e.g., String, Number, Boolean, undefined, null, Symbol, BigInt) are stored in the stack memory, which is fast and stores fixed-size data.
+
+When you assign a primitive value to a new variable, a copy of the value is created.
+Changing the value of one variable does not affect the original.
+*/
 //heap=>Non-Primitive
+/*
+-->Non-Primitive Types (Heap Memory)
+Non-primitive data types (e.g., Object, Array, Function) are stored in heap memory, which is used for dynamic and larger memory allocations.
+
+When you assign a non-primitive value (e.g., an object) to a new variable, a reference to the same memory location is created.
+Changing one reference affects the other because both refer to the same object.
+*/
 
 
 let myName = "Mamta"
