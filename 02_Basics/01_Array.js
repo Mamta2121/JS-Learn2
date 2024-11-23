113519a51f91b6b321562b38039f0046c0d9d390
Mamta2121 <mamtarajera2121@gmail.com>
2024-11-23 13:10:11 +0530

    Update 01_Array.js

--- 02_Basics/01_Array.js
+++ 02_Basics/01_Array.js
@@ -1,17 +1,17 @@
-//Array
+//Way to make an Array in JS
+
+//#1 Way -> Array Literal 
 const myArr = [0,1,2,3,4,5,] 
 //const myArr = [0,1,2,3,true,"mamta"]
 //Mix of datatype
 //index start from 0 => Not like myArr["one"], but instead use myArr[1]
 //shallow copy
 
+//#2 Way -> Using Array Constructor
 const myArr2 = new Array(1,2,3,4);
 console.log(myArr2);
 
-//Array methods
-
-//++++++++++++++++++++++ #1 +++++++++++++++++
-
+//Array Methods
 //push -> insert at the end 
 
 myArr.push(6);
@@ -43,7 +43,7 @@ console.log(myArr)
 console.log(myArr.includes(9));
 console.log(myArr.indexOf(3));
 
-const newArr = myArr.join();
+const newArr = myArr.join(); //converts array to a string, joining the elements by the separater provided as the parameter. BY DEFAULT -> ,
 
 console.log(newArr);   //data type of newArr is string
 console.log(myArr);
@@ -55,12 +55,12 @@ console.log(myArr);
 
 //slice //splice
 
-const myn1 = myArr.slice(1,3);  //range not included and no change in original array
+const myn1 = myArr.slice(1,3);  //second parameer index is not included and no change in original array
 
 console.log("A ", myArr);
 console.log(myn1);
 
-const myn2 = myArr.splice(1,3); //range included and also change  in original array
+const myn2 = myArr.splice(1,3); //second parameter index is  included and also change  in original array
 
 console.log("B ",myArr);
 console.log(myn2);
